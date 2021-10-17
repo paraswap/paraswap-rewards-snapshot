cd66330481f8f9ffd2074bcfc06218adf4a3f74b
Shresth Agrawal <shresthagrawal.31@gmail.com>
2021-10-17 14:54:24 +0200

    Add final SQL for airdrop values

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
-Snapshot Info:   
-    - Date: 8th October  
-    - Timestamp: 1633730399  
-    - BlockNumber:  
-        + Ethereum: 13380733  
-        + BSC: 11605446   
-        + Polygon: 19999042  
-        + Avalanche: 5389470  
+Snapshot Info:
+- Date: 8th October
+- Timestamp: 1633730399
+- BlockNumber:
+  - Ethereum: 13380733
+  - BSC: 11605446
+  - Polygon: 19999042
+  - Avalanche: 5389470
--- snapshot.sql
+++ snapshot.sql
@@ -107,67 +107,71 @@ FROM
           txorigin,
           txtarget,
           NULL AS UUID,
-          initiator as userAddress
-   FROM ethereum_v4 WHERE referrer IN ('argents','3')
+          initiator AS userAddress
+   FROM ethereum_v4
+   WHERE referrer IN ('argents',
+                      '3')
    UNION SELECT id,
-          augustus,
-          augustusversion,
-          side,
-          METHOD,
-          initiator,
-          beneficiary,
-          blocknumber,
-          blockhash,
-          txtimestamp,
-          referrer,
-          srctoken,
-          desttoken,
-          srcamount,
-          srcamountusd,
-          destamount,
-          destamountusd,
-          expectedamount,
-          ethereum_v4.txhash,
-          txgasprice,
-          txgasused,
-          txorigin,
-          txtarget,
-          NULL AS UUID,
-          CowSwapTxs.owner as userAddress
-    FROM ethereum_v4, CowSwapTxs
-    WHERE 
-        initiator IN ('0x9008d19f58aabd9ed0d60971565aa8510560ab41', '0x3328f5f2cecaf00a2443082b657cedeaf70bfaef') 
-        AND CowSwapTxs.txHash = ethereum_v4.txhash
-        AND CowSwapTxs.selltoken = ethereum_v4.srctoken
-    UNION SELECT id,
-          augustus,
-          augustusversion,
-          side,
-          METHOD,
-          initiator,
-          beneficiary,
-          blocknumber,
-          blockhash,
-          txtimestamp,
-          referrer,
-          srctoken,
-          desttoken,
-          srcamount,
-          srcamountusd,
-          destamount,
-          destamountusd,
-          expectedamount,
-          txhash,
-          txgasprice,
-          txgasused,
-          txorigin,
-          txtarget,
-          NULL AS UUID,
-          txorigin as userAddress
-    FROM ethereum_v4
-    WHERE 
-        initiator NOT IN ('0x9008d19f58aabd9ed0d60971565aa8510560ab41', '0x3328f5f2cecaf00a2443082b657cedeaf70bfaef')  
-        AND referrer NOT IN ('argents','3')
+                augustus,
+                augustusversion,
+                side,
+                METHOD,
+                initiator,
+                beneficiary,
+                blocknumber,
+                blockhash,
+                txtimestamp,
+                referrer,
+                srctoken,
+                desttoken,
+                srcamount,
+                srcamountusd,
+                destamount,
+                destamountusd,
+                expectedamount,
+                ethereum_v4.txhash,
+                txgasprice,
+                txgasused,
+                txorigin,
+                txtarget,
+                NULL AS UUID,
+                CowSwapTxs.owner AS userAddress
+   FROM ethereum_v4,
+        CowSwapTxs
+   WHERE initiator IN ('0x9008d19f58aabd9ed0d60971565aa8510560ab41',
+                       '0x3328f5f2cecaf00a2443082b657cedeaf70bfaef')
+     AND CowSwapTxs.txHash = ethereum_v4.txhash
+     AND CowSwapTxs.selltoken = ethereum_v4.srctoken
+   UNION SELECT id,
+                augustus,
+                augustusversion,
+                side,
+                METHOD,
+                initiator,
+                beneficiary,
+                blocknumber,
+                blockhash,
+                txtimestamp,
+                referrer,
+                srctoken,
+                desttoken,
+                srcamount,
+                srcamountusd,
+                destamount,
+                destamountusd,
+                expectedamount,
+                txhash,
+                txgasprice,
+                txgasused,
+                txorigin,
+                txtarget,
+                NULL AS UUID,
+                txorigin AS userAddress
+   FROM ethereum_v4
+   WHERE initiator NOT IN ('0x9008d19f58aabd9ed0d60971565aa8510560ab41',
+                           '0x3328f5f2cecaf00a2443082b657cedeaf70bfaef')
+     AND referrer NOT IN ('argents',
+                          '3')
    UNION SELECT id,
                 augustus,
                 augustusversion,
@@ -206,6 +210,7 @@ WHERE srctoken in
            AND desttoken = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
   AND NOT (desttoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
            AND srctoken = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2');
+
 
 CREATE OR REPLACE VIEW FilteredPolygonTXs AS
 SELECT *,
@@ -269,7 +274,8 @@ WHERE srctoken in
   AND desttoken in
     (SELECT tokenaddress
      FROM toppolygontokens)
-  AND txtimestamp < 1633730399 AND txtimestamp > 1614977606
+  AND txtimestamp < 1633730399
+  AND txtimestamp > 1614977606
   AND NOT (srctoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
            AND desttoken = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270')
   AND NOT (desttoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
@@ -338,7 +344,8 @@ WHERE srctoken in
   AND desttoken in
     (SELECT tokenaddress
      FROM topbsctokens)
-  AND txtimestamp < 1633730399 AND txtimestamp > 1614977606
+  AND txtimestamp < 1633730399
+  AND txtimestamp > 1614977606
   AND NOT (srctoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
            AND desttoken = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c')
   AND NOT (desttoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
@@ -381,7 +388,8 @@ WHERE srctoken in
   AND desttoken in
     (SELECT tokenaddress
      FROM topavalanchetokens)
-  AND txtimestamp < 1633730399 AND txtimestamp > 1614977606
+  AND txtimestamp < 1633730399
+  AND txtimestamp > 1614977606
   AND NOT (srctoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
            AND desttoken = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7')
   AND NOT (desttoken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
@@ -399,31 +407,42 @@ CREATE materialized VIEW AllFilteredTxs AS
    FROM FilteredAvalancheTXs);
 
 -- filter eligible users
+
 CREATE materialized VIEW EligibleUsers AS
-SELECT allfilteredtxs.useraddress From allfilteredtxs, 
-(SELECT distinct(useraddress)
-FROM userInfo
-WHERE ((network = 1
-        AND balance > 28000000000000000)
-       OR (network = 56
-           AND balance > 250000000000000000)
-       OR (network = 137
-           AND balance > 20000000000000000000)
-       OR (network = 43114
-           AND balance > 900000000000000000))
-  OR txcount > 50) as networkFilteredUsers
-  Where allfilteredtxs.useraddress = networkFilteredUsers.useraddress
-  Group by allfilteredtxs.useraddress having count(*) > 5;
+SELECT allfilteredtxs.useraddress
+FROM allfilteredtxs,
+
+  (SELECT distinct(useraddress)
+   FROM userInfo
+   WHERE ((network = 1
+           AND balance > 28000000000000000)
+          OR (network = 56
+              AND balance > 250000000000000000)
+          OR (network = 137
+              AND balance > 20000000000000000000)
+          OR (network = 43114
+              AND balance > 900000000000000000))
+     OR txcount > 50) AS networkFilteredUsers
+WHERE allfilteredtxs.useraddress = networkFilteredUsers.useraddress
+GROUP BY allfilteredtxs.useraddress
+HAVING count(*) > 5;
 
 -- Add indexes to optimise query
+
 CREATE INDEX EligibleUsers_useraddress ON EligibleUsers(useraddress);
 
+
 CREATE INDEX AllFilteredTxs_useraddress ON AllFilteredTxs(useraddress);
+
+
 CREATE INDEX AllFilteredTxs_srcamountusd ON AllFilteredTxs(srcamountusd);
+
+
 CREATE INDEX AllFilteredTxs_network ON AllFilteredTxs(network);
 
 -- create queries for points
-CREATE materialized VIEW txCountUserPoints as
+
+CREATE materialized VIEW txCountUserPoints AS
 SELECT allfilteredtxs.useraddress,
        CASE
            WHEN count(*) > 10 THEN 1
@@ -434,53 +453,66 @@ FROM allfilteredtxs,
 WHERE allfilteredtxs.useraddress = eligibleusers.useraddress
 GROUP BY allfilteredtxs.useraddress;
 
-CREATE materialized VIEW maxTxValueUserPoints as 
+
+CREATE materialized VIEW maxTxValueUserPoints AS
 SELECT allfilteredtxs.useraddress,
        CASE
-            when max(srcamountusd) > 100000 then 4
-            when max(srcamountusd) between 10000 and 100000 then 3
-            when max(srcamountusd) between 1000 and 10000 then 2
-            when max(srcamountusd) between 100 and 1000 then 1
-            else 0
+           WHEN max(srcamountusd) > 100000 THEN 4
+           WHEN max(srcamountusd) BETWEEN 10000 AND 100000 THEN 3
+           WHEN max(srcamountusd) BETWEEN 1000 AND 10000 THEN 2
+           WHEN max(srcamountusd) BETWEEN 100 AND 1000 THEN 1
+           ELSE 0
        END AS maxTxValuePoints
 FROM allfilteredtxs,
      eligibleusers
 WHERE allfilteredtxs.useraddress = eligibleusers.useraddress
 GROUP BY allfilteredtxs.useraddress;
 
-CREATE materialized VIEW userVolumeUserPoints as 
+
+CREATE materialized VIEW userVolumeUserPoints AS
 SELECT allfilteredtxs.useraddress,
        CASE
-            when sum(srcamountusd) > 100000 then 4
-            when sum(srcamountusd) between 10000 and 100000 then 3
-            when sum(srcamountusd) between 1000 and 10000 then 2
-            when sum(srcamountusd) between 100 and 1000 then 1
-            else 0
+           WHEN sum(srcamountusd) > 100000 THEN 4
+           WHEN sum(srcamountusd) BETWEEN 10000 AND 100000 THEN 3
+           WHEN sum(srcamountusd) BETWEEN 1000 AND 10000 THEN 2
+           WHEN sum(srcamountusd) BETWEEN 100 AND 1000 THEN 1
+           ELSE 0
        END AS userVolumePoints
 FROM allfilteredtxs,
      eligibleusers
 WHERE allfilteredtxs.useraddress = eligibleusers.useraddress
 GROUP BY allfilteredtxs.useraddress;
 
-CREATE materialized VIEW networkUserPoints as 
+
+CREATE materialized VIEW networkUserPoints AS
 SELECT allfilteredtxs.useraddress,
        CASE
-            when count(distinct network) > 1 then 1
-            else 0
+           WHEN count(DISTINCT network) > 1 THEN 1
+           ELSE 0
        END AS networkPoints
 FROM allfilteredtxs,
      eligibleusers
 WHERE allfilteredtxs.useraddress = eligibleusers.useraddress
 GROUP BY allfilteredtxs.useraddress;
 
-CREATE VIEW TotalPoints as
+
+CREATE VIEW TotalPoints AS
 SELECT txCountUserPoints.useraddress,
        (txCountUserPoints.txCountPoints + maxTxValueUserPoints.maxTxValuePoints + userVolumeUserPoints.userVolumePoints + networkUserPoints.networkPoints) AS sumPoints
 FROM txCountUserPoints,
      maxTxValueUserPoints,
      userVolumeUserPoints,
      networkUserPoints
-WHERE 
-    txCountUserPoints.useraddress = maxTxValueUserPoints.useraddress and 
-    txCountUserPoints.useraddress = userVolumeUserPoints.useraddress and
-    txCountUserPoints.useraddress = networkUserPoints.useraddress;
+WHERE txCountUserPoints.useraddress = maxTxValueUserPoints.useraddress
+  AND txCountUserPoints.useraddress = userVolumeUserPoints.useraddress
+  AND txCountUserPoints.useraddress = networkUserPoints.useraddress;
+
+
+SELECT useraddress AS address,
+       CASE
+           WHEN sumPoints > 7 THEN 8000
+           WHEN sumPoints <= 7
+                AND sumPoints > 3 THEN 6000
+           WHEN sumPoints <= 3 THEN 4000
+       END AS earnings
+FROM TotalPoints;
